8497c43300efab40d15bc57bb4c85e9249e35864
Sadya Hossain Sinthi <sadyasinthi1@gmail.com>
2025-05-05 10:20:30 -0400

    first commit

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import {
+    userCredits,
+    registerUser,
+    loginUser,
+    paymentStripe,
+    verifyStripe
+} from '../controllers/UserController.js'
+import authUser from '../middlewares/auth.js'
+
+const userRouter = express.Router()
+
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+userRouter.get('/credits', authUser, userCredits)
+
+userRouter.post('/pay-stripe', authUser, paymentStripe)
+userRouter.post('/verify-stripe', authUser, verifyStripe)
+
+export default userRouter
